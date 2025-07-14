8215bca1fa994d9e2a3ac1bfbb7e5230ccb0c773
JUANMANUEL48432750 <juanmanuel.55@icloud.com>
2025-07-14 18:01:08 -0500

    Update message.js

--- message.js
+++ message.js
@@ -1,13 +1,13 @@
-settime = () =>{
+settime = (payments ) =>{
 const randomeNumber = Math.floor(Math.random() * 10);
 const message = {
   greeding : ['good morning', 'good night', 'good eveing'],
   quaction:['drinking','doing','eating'],
   courage:['do it', 'finish it', 'complet it']
 }
+ te amo dios 
 
-
-genarateMeassage = () => {
+genarateMeassage = (hello world ) => {
   switch(randomeNumber){
     case 1:{
       console.log(`${message.greeding[0]} to you`);
@@ -21,21 +21,22 @@ genarateMeassage = () => {
       console.log(`${message.greeding[2]} to you`);
       break;
     }
+    breakdown 
     case 4:{
       console.log(`You can ${message.courage[0]} if you puth your mind in to it.`);
-      break;
+      break; break again??
     }
     case 5:{
       console.log(`You can ${message.courage[1]} if you puth your mind in to it.`);
       break;
     }
-    case 6:{
+    case 6:{ bit to bit understanding 
       console.log(`You can ${message.courage[2]} if you puth your mind in to it.`);
-      break;
+      break; im master on this 
     }
     case 7:{
       console.log(`what are you ${message.courage[0]} know.`);
-      break;
+      break;machine learning
     }
     case 8:{
       console.log(`what are you ${message.courage[1]} know.`);
@@ -46,7 +47,7 @@ genarateMeassage = () => {
       break;
     }
     default:{
-      console.log(`why are you sprised...`);
+      console.log(`log log log why are you sprised...`);
       break;
     }
     
@@ -59,12 +60,12 @@ let stratTime = new Date().getTime()
 
 let displayMessages = setInterval( () => {
   let currantTime = new Date() .getTime();
-  if(currantTime - stratTime > 30000){
+  if(currantTime - stratefree stratTime > 30000){
     clearInterval(displayMessages);
       return;
   } 
 
   settime();
-
-},4000)
+ codeeee
+},4000) +180020297392 $. 
 
